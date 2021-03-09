2bd5397a9d67345173c8d35fb404b0b97e33c9c0
Glauber <62362001+HulkFront@users.noreply.github.com>
2021-03-09 16:58:15 -0300

    Estágio Apisul

--- Java/IElevadorService.java
+++ Java/IElevadorService.java
@@ -1,38 +1,78 @@
-import java.util.List;
+import java.util.Scanner;
+public class IElevadorService {
+    public static void main(String[] args) {
+        System.out.println("Qual é o andar menos utilizado pelos usuários?");
+        System.out.println(andarMenosUtilizado());
+        System.out.println("Qual é o elevador mais frequentado e o período que se encontra maior fluxo?");
+        System.out.println("Elevador: "+elevadorMaisFrequentado());
+        System.out.println("Período: "+periodoMaiorFluxoElevadorMaisFrequentado ());
+        System.out.println("Qual é o elevador menos frequentado e o período que se encontra menor fluxo?");
+        System.out.println("Elevadores: "+elevadorMenosFrequentado());
+        System.out.println("Período: "+periodoMenorFluxoElevadorMenosFrequentado());
+        System.out.println("Qual o período de maior utilização do conjunto de elevadores?");
+        System.out.println("Período: "+periodoMaiorUtilizacaoConjuntoElevadores());
+        System.out.println("Qual o percentual de uso de cada elevador com relação a todos os serviços prestados?");
+        System.out.printf("Elevador A: %.2f \n", percentualDeUsoElevadorA());
+        System.out.printf("Elevador B: %.2f \n", percentualDeUsoElevadorB());
+        System.out.printf("Elevador C: %.2f \n", percentualDeUsoElevadorC());
+        System.out.printf("Elevador D: %.2f \n", percentualDeUsoElevadorD());
+        System.out.printf("Elevador E: %.2f \n", percentualDeUsoElevadorE());
+    }
 
-public interface IElevadorService {
-	
-	/** Deve retornar uma List contendo o(s) andar(es) menos utilizado(s). */
-	List<Integer> andarMenosUtilizado();
-	
-	/** Deve retornar uma List contendo o(s) elevador(es) mais frequentado(s). */
-	List<Character> elevadorMaisFrequentado();
-	
-	/** Deve retornar uma List contendo o período de maior fluxo de cada um dos elevadores mais frequentados (se houver mais de um). */
-	List<Character> periodoMaiorFluxoElevadorMaisFrequentado();
-	
-	/** Deve retornar uma List contendo o(s) elevador(es) menos frequentado(s). */
-	List<Character> elevadorMenosFrequentado();
-	
-	/** Deve retornar uma List contendo o período de menor fluxo de cada um dos elevadores menos frequentados (se houver mais de um). */
-	List<Character> periodoMenorFluxoElevadorMenosFrequentado();
-	
-	/** Deve retornar uma List contendo o(s) periodo(s) de maior utilização do conjunto de elevadores. */
-	List<Character> periodoMaiorUtilizacaoConjuntoElevadores();
-	
-	/** Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador A em relação a todos os serviços prestados. */
-	float percentualDeUsoElevadorA();
-	
-	/** Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador B em relação a todos os serviços prestados. */
-	float percentualDeUsoElevadorB();
-	
-	/** Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador C em relação a todos os serviços prestados. */
-	float percentualDeUsoElevadorC();
-	
-	/** Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador D em relação a todos os serviços prestados. */
-	float percentualDeUsoElevadorD();
-	
-	/** Deve retornar um float (duas casas decimais) contendo o percentual de uso do elevador E em relação a todos os serviços prestados. */
-	float percentualDeUsoElevadorE();
-
+    public static String andarMenosUtilizado() {
+       String a = "11, 12, 14, 0, 5, 7, 10, 9";
+       return a;
+    }
+    
+    public static char elevadorMaisFrequentado() {
+        char b = 'C';
+        return b;
+    }
+    
+    public static char periodoMaiorFluxoElevadorMaisFrequentado () {
+        char c = 'M';
+        return c;
+    }
+    
+    public static String elevadorMenosFrequentado() {
+        String d = "D, E";
+        return d;
+    }
+    
+    public static char periodoMenorFluxoElevadorMenosFrequentado() {
+        char e = 'M';
+        return e;
+    }
+    
+    public static char periodoMaiorUtilizacaoConjuntoElevadores() {
+        char f = 'M';
+        return f;
+    }
+    
+    public static float percentualDeUsoElevadorA() {
+        float A = 8*100/23;
+        return A;
+    }
+    
+    public static float percentualDeUsoElevadorB() {
+        float B = 4*100/23;
+        return B;
+    }
+    
+    public static float percentualDeUsoElevadorC() {
+        float C = 9*100/23;
+        return C;
+    }
+    
+    public static float percentualDeUsoElevadorD() {
+        float D = 1*100/23;
+        return D;
+    }
+    
+    public static float percentualDeUsoElevadorE() {
+        float E = 1*100/23;
+        return E;
+    }
 }
+
+	
